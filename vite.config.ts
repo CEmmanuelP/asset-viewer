7dd23445fc6c6c2efd8a4a11521bfe9997fe691e
Yujin Kim <yujin@eazel.net>
2024-11-25 18:03:43 +0900

    🐛 Change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/asset-viewer",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
