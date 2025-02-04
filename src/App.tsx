c89f6385d248326cbbbd9c549f74c42a80ad31c7
eugene <chrisp3@daum.net>
2025-02-04 13:53:09 +0900

    🐛 (SceneViewer): Fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
+import { ReactNode } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Artwork from "./components/Artwork"
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import ScenePage from "./pages/Scene"
 
+interface DefaultLayoutProps {
+  children: ReactNode
+}
+
+const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <div className="container mx-auto px-4">{children}</div>
+      </main>
+    </div>
+  )
+}
+
 function App() {
   const basename = import.meta.env.DEV ? "" : "/asset-viewer"
 
   return (
     <BrowserRouter basename={basename}>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1">
-          <div className="container mx-auto px-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/artworks/:id" element={<Artwork />} />
-              <Route path="/scene/:id" element={<ScenePage />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <Home />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/artworks/:id"
+          element={
+            <DefaultLayout>
+              <Artwork />
+            </DefaultLayout>
+          }
+        />
+        <Route path="/scene/:id" element={<ScenePage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
